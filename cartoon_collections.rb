12f342104ab461f16505fef4a2f391708bcc23a4
Katie Blizzard <kblizzard11@gmail.com>
2019-02-08 02:53:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(array)
-  array.each_with_index { |x, index| }
+  array.each_with_index { |x, index| 
+    puts "#{index}. x"
+  }
 end
 
 def summon_captain_planet# code an argument here
