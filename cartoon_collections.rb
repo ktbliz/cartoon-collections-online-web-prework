bad25685875f5cf1c67b085389cf0f13603aa704
Katie Blizzard <kblizzard11@gmail.com>
2019-02-08 03:02:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(array)
   }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.find 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
